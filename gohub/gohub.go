409a07354df6b63c3e470f9b6916465e9162d654
limpdev <150757061+limpdev@users.noreply.github.com>
2025-02-19 13:39:07 -0600

    Update gohub.go

--- gohub/gohub.go
+++ gohub/gohub.go
@@ -35,11 +35,11 @@ func main() {
  	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/clipb.css">
 	</head>
 	<body>
-	<script src="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/prismHL.js"></script>
-	<script src="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/clipb.js"></script>
+ 	<script src="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/clipb.js"></script>
 	<article class="markdown-body">
 	%s
 	</article>
+ 	<script src="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/prismHL.js"></script>
 	</body>
 	</html>`, htmlContent)
 
