568add80c239b43610d1ed5ae491658e7332389d
limpdev <drewgorbet2020@gmail.com>
2025-02-21 18:58:10 -0600

    Update modified files

--- gohub/gohub.go
+++ gohub/gohub.go
@@ -34,11 +34,11 @@ func main() {
 	<link rel="stylesheet"type="text/css"href="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/clipb.css"/>
 	</head>
 	<body>
-	<script src="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/prismHL.js"></script>
-	<script src="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/clipb.js"></script>
 	<article class="markdown-body">
 	%s
 	</article>
+	<script src="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/prismHL.js"></script>
+	<script src="https://cdn.jsdelivr.net/gh/limpdev/limpbin@main/css/clipb.js"></script>
 	</body>
 	</html>`, htmlContent)
 
